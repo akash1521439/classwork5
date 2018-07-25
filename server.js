dda650cf0d9d31823dd31dc1848232a952dbc362
Mahbubur Rahman Akash <1521439@iub.edu.bd>
2018-07-25 04:47:24 +0000

    class work 8

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ var http = require('http');
   var express = require('express');
   var app = express();
   var bodyParser = require("body-parser");
+  
+  var mongo = require('mongodb');
+  //for local
+  // var db. uri ="mongodb://localhost:27017";
+  //for c9
+
+  
+ var db, uri = "mongodb://" + process.env.IP + "/28017";
+   
+  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+    if(err){console.log("Could not connect to MongoDB")
+    }
+    else {
+      db = client.db('node-cw8');
+     
+    }
+  });
+  var save = function(form_data){
+    db.createCollection('users', function(err, collection){});
+    var collection = db.collection('users');
+    collection.save(form_data);
+  }
+  
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({entended:true}));
   var server = http.Server(app);
@@ -21,6 +44,8 @@ var http = require('http');
   
   app.post('/form', function(req, res){
     console.log(req.body);
+    save(req.body);
+    res.status('200');
   });
   
   server.listen(process.env.PORT, process.env.IP, function(){
