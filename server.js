e1f30bb56d11c3770003c85893a4858eea9a1c32
Mahbubur Rahman Akash <1521439@iub.edu.bd>
2018-07-30 03:19:22 +0000

    classWork 9

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ var http = require('http');
   var express = require('express');
   var app = express();
   var bodyParser = require("body-parser");
+  var mongoose = require('mongoose');
+  mongoose.connect("mongodb://localhost:27017/node-cw8");
   
-  var mongo = require('mongodb');
+  //var mongo = require('mongodb');
   //for local
   // var db. uri ="mongodb://localhost:27017";
   //for c9
-
+ mongoose.connection.on('error',function(){
+   console.log('Could not connect to mongodb');
+ });
+ 
+ var userSchema = new mongoose.Schema({
+ name: {
+   type: String,
+   required: "Name is required"
+ },
+ email: String
+ });
+ var User = mongoose.model('User', userSchema);
   
- var db, uri = "mongodb://" + process.env.IP + "/28017";
+ //var db, uri = "mongodb://" + process.env.IP + "/28017";
    
-  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+ /* mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
     if(err){console.log("Could not connect to MongoDB")
     }
     else {
@@ -19,6 +32,7 @@ var http = require('http');
      
     }
   });
+  */
   var save = function(form_data){
     db.createCollection('users', function(err, collection){});
     var collection = db.collection('users');
@@ -44,8 +58,16 @@ var http = require('http');
   
   app.post('/form', function(req, res){
     console.log(req.body);
-    save(req.body);
-    res.status('200');
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if (err)
+        return res.status(400).json({message: "could not save user"
+      });
+    
+    res.status(200).json(data);
+      })
+    //save(req.body);
+    //res.status('200');
   });
   
   server.listen(process.env.PORT, process.env.IP, function(){
